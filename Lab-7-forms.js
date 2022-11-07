9b7b0bd0546221ca689d17127d80f110f2ef2c11
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-11-07 11:29:06 -0500

    Completed Lab 7 + extra challenge

--- Lab-7-forms.js
+++ Lab-7-forms.js
@@ -10,4 +10,69 @@ var shipInfo = {
 
 window.onload = function () {
   let formHandle = document.forms.form_ship;
+  let thankYouMsg = document.getElementById("thanks_msg");
+  let thanksCustomer = document.getElementById("thanksCustomer");
+  let thanksPC = document.getElementById("thanksPC");
+  let speedF = formHandle.f_speed;
+  let thanksSpeed = document.getElementById("thanksSpeed");
+  let thanksCost = document.getElementById("thanksCost");
+
+  formHandle.onsubmit = processForm;
+
+  function processForm() {
+    let nameValue = formHandle.f_Name;
+    console.log(nameValue.value);
+
+    let idValue = formHandle.f_Id;
+    console.log(idValue.value);
+
+    let postCodeValue = formHandle.f_pc;
+    console.log(postCodeValue.value);
+
+    let speedValue = speedF.options[speedF.selectedIndex].text;
+    console.log(speedValue);
+
+    let costValue = formHandle.f_speed;
+    console.log(costValue.value);
+
+    //SHIPPING LEVEL
+
+    // VALIDATE NAME
+    if (nameValue.value === "") {
+      nameValue.style.background = "red";
+      nameValue.focus();
+      return false;
+    }
+
+    //VALIDATE CUSTOMER ID
+    if (idValue.value === "") {
+      idValue.style.background = "red";
+      idValue.focus();
+      return false;
+    }
+
+    //VALIDATE POSTAL CODE
+    if (postCodeValue.value === "") {
+      postCodeValue.style.background = "red";
+      postCodeValue.focus();
+      return false;
+    }
+
+    //show ty msg (IF VALID, SHOW BOTTOM)
+    if (
+      nameValue.value !== "" &&
+      idValue.value !== "" &&
+      postCodeValue.value != ""
+    ) {
+      formHandle.style.display = "none";
+      thankYouMsg.style.display = "block";
+
+      thanksCustomer.innerHTML = `${nameValue.value} (Customer # ${idValue.value})`;
+      thanksPC.innerHTML = postCodeValue.value;
+      thanksSpeed.innerHTML = speedValue;
+      thanksCost.innerHTML = costValue.value;
+    }
+
+    return false;
+  }
 };
