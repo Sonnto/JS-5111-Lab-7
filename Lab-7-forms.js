0928ad1786f139110758d2353788146b3cb96edd
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-11-07 10:14:09 -0500

    form validation html and js start

---
+++ Lab-7-forms.js
@@ -0,0 +1,13 @@
+/* LAB 8 - SHIPPING FORM */
+//Order Shipping object (for extra EXTRA challenge, otherwise, ignore it)
+var shipInfo = {
+  cid: 0,
+  name: "",
+  pc: "",
+  speed: "",
+  cost: 0,
+};
+
+window.onload = function () {
+  let formHandle = document.forms.form_ship;
+};
